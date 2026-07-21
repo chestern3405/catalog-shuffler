784bbfd8c15389f10213ddf371f6754adf523459
chestern3405 <87405860+chestern3405@users.noreply.github.com>
2026-07-20 22:03:36 -0500

    Update UPSTREAM_MANIFESTS with new URL version

--- server.js
+++ server.js
@@ -15,7 +15,7 @@
 
 // 1) Manifest URLs of the addons whose catalogs you want aggregated:
 const UPSTREAM_MANIFESTS = [
-  "https://bingecat.com/stremio/fc240269-f369-43c4-ae76-1e95bfc480ee/nuvio/manifest.json?bcv=201",
+  "https://bingecat.com/stremio/fc240269-f369-43c4-ae76-1e95bfc480ee/nuvio/manifest.json?bcv=217",
   "https://aiometadata.viren070.me/stremio/5ec91dc1-27ba-4165-8d77-c42d9b77cd35/manifest.json",
   // "https://toptoday.llamayu.com/landscapeTags=true|landscapeLogos=false|landscapeRanked=false|portraitTags=true|portraitLogos=false|portraitRanked=false|posterLang=en|digitalOnly=true|listLang=en/manifest.json",
 ];
