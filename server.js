f93857ea79cf2fbbbe624f0ca737750b49780a50
chestern3405 <87405860+chestern3405@users.noreply.github.com>
2026-07-20 21:13:16 -0500

    Update manifest URLs in server.js
    Comment out existing manifest URLs and add a new one.

--- server.js
+++ server.js
@@ -15,8 +15,9 @@
 
 // 1) Manifest URLs of the addons whose catalogs you want aggregated:
 const UPSTREAM_MANIFESTS = [
-  "https://bingecat.com/stremio/fc240269-f369-43c4-ae76-1e95bfc480ee/nuvio/manifest.json?bcv=78",
-  "https://aiometadata.viren070.me/stremio/5ec91dc1-27ba-4165-8d77-c42d9b77cd35/manifest.json",
+  // "https://bingecat.com/stremio/fc240269-f369-43c4-ae76-1e95bfc480ee/nuvio/manifest.json?bcv=78",
+  // "https://aiometadata.viren070.me/stremio/5ec91dc1-27ba-4165-8d77-c42d9b77cd35/manifest.json",
+  "https://toptoday.llamayu.com/landscapeTags=true|landscapeLogos=false|landscapeRanked=true|portraitTags=true|portraitLogos=false|portraitRanked=true|posterLang=en|digitalOnly=true|listLang=en/manifest.json",
 ];
 
 const ADDON_ID = "org.chase.catalog-shuffler";
